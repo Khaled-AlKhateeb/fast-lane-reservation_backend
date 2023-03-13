77e162cf2108d47eb1c6d0afaac859fdadb22270
Degisew <jegisew21@gmail.com>
2023-03-13 22:29:53 +0300

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users do
+  resources :vehicles do
+    resources :reservations
+  end
+
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
