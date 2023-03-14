1c65718a6bdf65a776d3b320d4d198c05f2eee1b
Kenny Egun <kennyegun240@gmail.com>
2023-03-14 20:20:24 +0100

    Fix git author name error

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,4 +3,5 @@ class Vehicle < ApplicationRecord
   has_many :reservations
 
   validates :name, :model, :price, :description, :image, :horse_power, presence: true
+  validates :price, numericality: { only_integer: true, greater_than: 0 }, presence: true
 end
