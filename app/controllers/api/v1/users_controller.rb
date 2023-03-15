0464ba74a65455102aa57e435a2ebfbb00210999
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2023-03-15 23:12:03 +0300

    Fix Rubocop

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_api_v1_user, only: %i[ show update destroy ]
+  before_action :set_api_v1_user, only: %i[show update destroy]
 
   # GET /api/v1/users
   def index
@@ -39,13 +39,14 @@ class Api::V1::UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_api_v1_user
-      @api_v1_user = Api::V1::User.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def api_v1_user_params
-      params.fetch(:api_v1_user, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_api_v1_user
+    @api_v1_user = Api::V1::User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def api_v1_user_params
+    params.fetch(:api_v1_user, {})
+  end
 end
