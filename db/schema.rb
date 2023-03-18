0ab4d9f9b6bbbc4b72d848414889b8262dc65afb
Kenny Egun <kennyegun240@gmail.com>
2023-03-18 17:54:17 +0100

    Fix linters

--- db/schema.rb
+++ db/schema.rb
@@ -25,16 +25,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_17_113831) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "f_name"
+    t.string "name"
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.string "email"
   end
 
   create_table "vehicles", force: :cascade do |t|
