4ce336e4375b9887384f3ddc9be47dc6bf2dfc79
Degisew <jegisew21@gmail.com>
2023-03-23 00:15:09 +0300

    Added case sensitivity validations to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   # validation
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false}
   validates :password, presence: true, length: { minimum: 6 }
 
   # Associations
