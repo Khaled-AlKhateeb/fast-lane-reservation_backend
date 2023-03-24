347a7ed277083b35e376eac96a0a0adde3198d32
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2023-03-24 16:40:10 +0300

    Master.key and commented code fix

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -2,16 +2,10 @@ class Api::V1::ReservationsController < ApplicationController
   def index
     @reservations = current_user.reservations.to_json(include: :vehicle)
     render json: @reservations, status: 200
-    # @kenny
   end
 
   def create
-    # @vehicle = Vehicle.find(params[:id])
-    # @user = User.find(params[:id])
     @reservation = current_user.reservations.new(reservation_params)
-    # @reservation = Reservation.new(reservation_params)
-    # @reservation.user_id = @user.id
-    # @reservation.vehicle_id = @vehicle.id
 
     if @reservation.save!
       puts current_user.email, 'C user'
